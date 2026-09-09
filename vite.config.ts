a8e994e063152dfe809d4e7fac0f37a29bed72f0
Vlad G <48860231+vladi375@users.noreply.github.com>
2026-09-09 14:03:13 +0400

    chore: remove unused

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: '/lunazo-partners/',
     plugins: [react()],
-    test: {
-        environment: 'jsdom',
-        setupFiles: './src/test/setup.ts',
-        include: ['src/**/*.test.{ts,tsx}'],
-    },
 });
